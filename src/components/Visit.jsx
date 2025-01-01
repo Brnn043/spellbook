7aa0ab405fc91caae09e5fb77c87ab831e26a910
Brnn <raksakul2442@gmail.com>
2025-01-02 03:43:37 +0700

    feat: navigate to another page button

--- src/components/Visit.jsx
+++ src/components/Visit.jsx
@@ -1,17 +1,35 @@
+'use client';
+
+import Link from 'next/link';
+
 export default function Visit({ name, setOpen }) {
-
-    const handleNotyet = () => {
+    const handleClick = () => {
         setOpen(false);
-        console.log('change to false');
+    };
 
-    }
-    return <>
-        <div className="fixed w-full h-full flex justify-center items-center z-30">
-            <div className="bg-amber-300 flex flex-col justify-center items-center p-6 border-2 border-black rounded-xl space-y-4">
-                <p>Want to visit {name} ?</p>
-                <button className="bg-white hover:bg-slate-200 border-2 border-black px-4 py-1 rounded-lg text-center w-24">yes</button>
-                <button className="bg-white hover:bg-slate-200 border-2 border-black px-4 py-1 rounded-lg text-center w-24" onClick={handleNotyet}>not yet</button>
+    return (
+        <>
+            <div className="fixed w-full h-full flex justify-center items-center z-30">
+                <div className="bg-amber-300 flex flex-col justify-center items-center p-6 border-2 border-black rounded-xl space-y-4">
+                    <p>Want to visit {name}?</p>
+                    <div className="flex space-x-4">
+                        <Link href={`/${name}`}>
+                            <button
+                                className="bg-white hover:bg-slate-200 border-2 border-black px-4 py-1 rounded-lg text-center w-24"
+                                onClick={handleClick}
+                            >
+                                Yes
+                            </button>
+                        </Link>
+                        <button
+                            className="bg-white hover:bg-slate-200 border-2 border-black px-4 py-1 rounded-lg text-center w-24"
+                            onClick={handleClick}
+                        >
+                            Not yet
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div >
-    </>
+        </>
+    );
 }
