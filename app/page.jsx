a78253f47522d2d7fb719455c5077c22188cd818
Brnn <raksakul2442@gmail.com>
2025-01-02 03:11:48 +0700

    refactor: MoveButton

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+import MoveButton from '@/components/MoveButton';
 import Experience from './Experience';
 import { KeyboardControls, OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
@@ -6,10 +7,6 @@ import { useState } from 'react';
 
 export default function Page() {
   const [controls, setControls] = useState({ forward: false, backward: false });
-
-  const handleControl = (action, state) => {
-    setControls((prev) => ({ ...prev, [action]: state }));
-  };
 
   return (
     <>
@@ -33,29 +30,7 @@ export default function Page() {
         </Canvas>
       </KeyboardControls>
 
-
-      <div className="fixed bottom-4 left-0 right-0 flex justify-center space-x-4 z-10">
-        <button
-          tabIndex={-1}
-          onMouseDown={() => handleControl('backward', true)}
-          onMouseUp={() => handleControl('backward', false)}
-          onTouchStart={() => handleControl('backward', true)}
-          onTouchEnd={() => handleControl('backward', false)}
-          className="w-28 h-12 flex items-center justify-center text-center text-base bg-white text-black border-2 border-black rounded-full hover:bg-gray-100 focus:outline-none"
-        >
-          Backward
-        </button>
-        <button
-          tabIndex={-1}
-          onMouseDown={() => handleControl('forward', true)}
-          onMouseUp={() => handleControl('forward', false)}
-          onTouchStart={() => handleControl('forward', true)}
-          onTouchEnd={() => handleControl('forward', false)}
-          className="w-28 h-12 flex items-center justify-center text-center text-base bg-white text-black border-2 border-black rounded-full hover:bg-gray-100 focus:outline-none"
-        >
-          Forward
-        </button>
-      </div>
+      <MoveButton setControls={setControls} />
     </>
   );
 }
---
+++ src/components/MoveButton.jsx
@@ -0,0 +1,27 @@
+export default function MoveButton({ setControls }) {
+    const handleControl = (action, state) => {
+        setControls((prev) => ({ ...prev, [action]: state }));
+    };
+    return <div className="fixed bottom-4 left-0 right-0 flex justify-center space-x-4 z-10">
+        <button
+            tabIndex={-1}
+            onMouseDown={() => handleControl('backward', true)}
+            onMouseUp={() => handleControl('backward', false)}
+            onTouchStart={() => handleControl('backward', true)}
+            onTouchEnd={() => handleControl('backward', false)}
+            className="w-28 h-12 flex items-center justify-center text-center text-base bg-white text-black border-2 border-black rounded-full hover:bg-gray-100 focus:outline-none"
+        >
+            Backward
+        </button>
+        <button
+            tabIndex={-1}
+            onMouseDown={() => handleControl('forward', true)}
+            onMouseUp={() => handleControl('forward', false)}
+            onTouchStart={() => handleControl('forward', true)}
+            onTouchEnd={() => handleControl('forward', false)}
+            className="w-28 h-12 flex items-center justify-center text-center text-base bg-white text-black border-2 border-black rounded-full hover:bg-gray-100 focus:outline-none"
+        >
+            Forward
+        </button>
+    </div>
+}
