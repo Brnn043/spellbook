4f9ad166461ec62238161b0bbca76e73a6bb3d39
Brnn <raksakul2442@gmail.com>
2025-01-04 02:44:51 +0700

    feat: comp modal

--- src/components/project/Modal.jsx
+++ src/components/project/Modal.jsx
@@ -83,50 +83,24 @@ export const GameDev = ({ setOpen }) => {
 
 
 export const Competitive = ({ setOpen }) => {
-    const sentences = [
-        "TOI18",
-        "Larngear passport",
-        "I am an enthusiastic, hardworking, and passionate individual with a love for creativity.",
-        "I am someone who quickly adapts to new challenges and always seeks innovative solutions.",
-        "When faced with difficulties, I rely on my dedication and commitment to push through and achieve success.",
-        'My motto is "Commitment is the key to success."'
+    const data = [
+        {
+            title: "ICPC Thailand 2024, Chulalongkorn representatives",
+            images: ['/project/ICPC/1.webp', '/project/ICPC/2.webp'],
+            description: 'We are Chulalongkorn University representatives attending the International collegiate programming contest in the name of natteeInwza007. This is a competition about advanced competitive programming that combines students from all universities in Thailand.',
+            tags: ['ICPCThailand2024', 'natteeInwza007', 'C++'],
+            viewMore: null
+        },
+        {
+            title: "Bronze medal, Thailand Olympiad in Informatics 18",
+            images: ['/project/TOI18/1.webp', '/project/TOI18/2.webp'],
+            description: 'I got a Bronze medal in the Thailand Olympiad in Informatics 18. This is the competition about solving advanced competitive programming.',
+            tags: ['TOI18', 'PosnComputer', 'C++'],
+            viewMore: null
+        }
     ];
 
-    const [sentenceIndex, setSentenceIndex] = useState(0);
-
-    const showNextSentences = () => {
-        setSentenceIndex((prevIndex) => {
-            const nextIndex = prevIndex + 2;
-            return nextIndex < sentences.length ? nextIndex : prevIndex;
-        });
-        if (sentenceIndex + 1 === sentences.length - 1) setOpen(false)
-    };
-
-    return (
-        <>
-            <div className="fixed w-full h-full flex justify-center items-center z-30 bg-thisBlack bg-opacity-50">
-                <div className="bg-white border-2 border-thisBlack rounded-xl p-6 w-3/4 max-w-xl flex flex-col space-y-2 md:flex-row md:space-x-4 relative">
-                    <img src="/Beam.webp" className="w-48" alt="Beam" />
-                    <div>
-                        <p className="text-base">
-                            {sentences[sentenceIndex]}
-                        </p>
-                        <p className="text-base mt-2">
-                            {sentences[sentenceIndex + 1]}
-                        </p>
-                    </div>
-                    <div className="flex justify-center items-center pt-4 mt-4">
-                        <button
-                            onClick={showNextSentences}
-                            className="bg-blue-500 hover:bg-blue-400 text-white border-2 border-black px-4 py-2 rounded-lg"
-                        >
-                            Next
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+    return <ProjectModal title={'Competitive Programming'} setOpen={setOpen} data={data} />
 }
 
 export const Others = ({ setOpen }) => {
