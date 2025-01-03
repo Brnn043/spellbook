2f4c26bf02a977796ad2d5a38dc3a54462766116
Brnn <raksakul2442@gmail.com>
2025-01-03 12:21:49 +0700

    feat: visit by open new tab

--- src/components/Visit.jsx
+++ src/components/Visit.jsx
@@ -14,7 +14,11 @@ export default function Visit({ name, setOpen }) {
                 <div className="bg-amber-300 flex flex-col justify-center items-center p-6 border-2 border-black rounded-xl space-y-4">
                     <p>Want to visit {name}?</p>
                     <div className="flex space-x-4">
-                        <Link href={`/${name}`}>
+                        <Link
+                            href={`/${name}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
                             <button
                                 className="bg-white hover:bg-slate-200 border-2 border-black px-4 py-1 rounded-lg text-center w-24"
                                 onClick={handleClick}
